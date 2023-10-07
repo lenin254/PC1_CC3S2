bf2a28a92a1f46e5040489d492f2d7d8998cf174
lenin254 <leninxarebalo18@hotmail.com>
2023-10-06 23:01:42 -0500

    actulizado

--- app.rb
+++ app.rb
@@ -52,8 +52,14 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+      when :win
+         redirect '/win'
+      when :lose
+         redirect '/lose'
+      else 
+         erb :show
+    end
   end
   
   get '/win' do
